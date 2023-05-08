ac2a4481c54d6b2f70f3d4eb134a42ed23237dc3
HouariZegai <houarizegai14@gmail.com>
2023-05-08 22:39:07 +0200

    Feat: evaluate the total price when bought no book

--- src/test/java/com/katas/developmentbooks/service/BookOrderTest.java
+++ src/test/java/com/katas/developmentbooks/service/BookOrderTest.java
@@ -3,11 +3,14 @@ package com.katas.developmentbooks.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class BookOrderTest {
 
     @Test
     void buyNoBookTotalZero() {
         BookOrderService bookOrderService = new BookOrderService();
+        assertEquals(0d, bookOrderService.getTotalPrice());
     }
 }
