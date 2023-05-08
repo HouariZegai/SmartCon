77db7790cea00eaebd213d31ad99e2a87ae75486
HouariZegai <houarizegai14@gmail.com>
2023-05-08 22:55:34 +0200

    Fix: made the buy one book test pass

--- src/main/java/com/katas/developmentbooks/service/BookOrderService.java
+++ src/main/java/com/katas/developmentbooks/service/BookOrderService.java
@@ -2,10 +2,18 @@ package com.katas.developmentbooks.service;
 
 public class BookOrderService {
 
+    private static final double SINGLE_BOOK_PRICE = 50d;
+
+    private Book book;
+
     public double getTotalPrice() {
+        if(book != null)
+            return SINGLE_BOOK_PRICE;
+
         return 0d;
     }
 
     public void addBook(Book book) {
+        this.book = book;
     }
 }
